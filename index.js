57c8e5ac22aefe3cfce75c6c57cebcc6243c7443
Muhammad Sabbir Khan Alif <muhammadsabbirkhanalif@gmail.com>
2024-12-12 17:28:19 +0000

    Added a new route for deleting vocabularies by filtering with id's

--- index.js
+++ index.js
@@ -139,6 +139,13 @@ async function run() {
       const result = await vocabularyCollection.updateOne(filter, updatedDoc, options);
       res.send(result);
     })
+
+    app.delete('/delete_vocabulary/:id', async(req, res) => {
+      const id = req?.params?.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await vocabularyCollection.deleteOne(query);
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
 
