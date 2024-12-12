5ca02e86b3c4b5d5a3f7768c2b6e4d7eff3dcff4
Muhammad Sabbir Khan Alif <muhammadsabbirkhanalif@gmail.com>
2024-12-12 14:49:44 +0000

    Added a new route for deleting lessons by filtering with _id

--- index.js
+++ index.js
@@ -98,6 +98,13 @@ async function run() {
       const result = await lessonCollection.updateOne(filter, updatedDoc, options);
       res.send(result);
     })
+
+    app.delete('/delete_lesson/:id', async(req, res) => {
+      const id = req?.params?.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await lessonCollection.deleteOne(query);
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
 
